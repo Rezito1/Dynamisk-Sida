fd8f6f7415e680876293860f58669353519529df
Rezito1 <130737539+Rezito1@users.noreply.github.com>
2026-03-26 16:41:13 +0100

    Uppdaterade Home och About sidor lite grann

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,17 @@
+import React from "react";
 
-
-export default function About(){
-    return (
-        <div>
-            <h1>About Page</h1>
-        </div>
-    )
+export default function About() {
+  return (
+    <div>
+      <h1>Om mig</h1>
+      <p>Hej! Jag heter Reza Nowrozi och är en Fullstack .NET-utvecklare.</p>
+      <p>
+        Jag gillar att skapa webbapplikationer med React och C#. 
+        Jag har erfarenhet från både frontend och backend, och tycker om att lära mig nya tekniker.
+      </p>
+      <p>
+        På fritiden gillar jag träning, programmering och att experimentera med nya projekt.
+      </p>
+    </div>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
+import React from "react";
 
-export default function Home(){
-    return (
-        <div>
-            <h1>Home Page</h1>
-            <p>Hello Reza</p>
-        </div>
-    )
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1>Hej, jag är Reza!</h1>
+      <p>
+        Välkommen till min personliga sida! Jag är en Fullstack .NET-utvecklare.
+      </p>
+      <p>
+        Utforska mina projekt och mitt CV i menyn ovan.
+      </p>
+    </div>
+  );
 }
